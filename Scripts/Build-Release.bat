7426165098110d51769d2822cfffe0766d9bd252
puk06 <86549420+puk06@users.noreply.github.com>
2026-06-22 21:58:48 +0900

    Fix script to remove built files

--- Scripts/Build-Release.bat
+++ Scripts/Build-Release.bat
@@ -2,7 +2,11 @@ set ProjectDirectory=%~dp0../RealtimePPUR/
 set ProjectFile=%ProjectDirectory%RealtimePPUR.csproj
 set BuildTargetDirectory=%ProjectDirectory%bin/build
 
-@REM # BUILD
+@REM Remove built files in target directory
+rmdir "%BuildTargetDirectory%" /s /q
+
+@REM Build
 dotnet publish "%ProjectFile%" -c Release -r win-x64 -o "%BuildTargetDirectory%"
 
+@REM Remove Unnecessary Files
 node RemoveUnnecessaryFiles.js "%BuildTargetDirectory%"
