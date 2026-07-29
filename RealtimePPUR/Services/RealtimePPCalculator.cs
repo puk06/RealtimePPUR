82d05d865aa7b49fece785ee501c2b9ee6a6b1ab
puk06 <86549420+puk06@users.noreply.github.com>
2026-07-29 17:43:10 +0900

    v2.2.1

--- RealtimePPUR/Services/RealtimePPCalculator.cs
+++ RealtimePPUR/Services/RealtimePPCalculator.cs
@@ -16,7 +16,7 @@ namespace RealtimePPUR.Services;
 
 public class RealtimePPCalculator
 {
-    public static readonly string CURRENT_VERSION = "v2.2.0";
+    public static readonly string CURRENT_VERSION = "v2.2.1";
 
     public event Action? OnCalculate = null;
     private MemoryReader MemoryReader { get; } = new();
